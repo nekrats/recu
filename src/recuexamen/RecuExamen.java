2c14562661a39b1b5680aa716e51c324fc366686
stark <stark@DESKTOP-KGMMH3C>
2019-01-14 17:38:53 +0100

    Subimos

---
+++ src/recuexamen/RecuExamen.java
@@ -0,0 +1,20 @@
+
+package recuexamen;
+
+public class RecuExamen {
+
+      public static void main(String[] args) {
+        int y;
+        System.out.println("Aquest es el main");
+    }
+    public static void hola(){
+        System.out.println("Això es un hola");
+    }
+    public static void meitat(){
+        System.out.println("Aquest està a la meitat");
+    }
+    public static void fin(){
+        System.out.println("Això es el fi del projecte");
+    }
+    
+}
