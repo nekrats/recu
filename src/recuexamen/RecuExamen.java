8dc00eb7d6ffdd45f549b30269f44355e0d2a973
nekrats <36659680+nekrats@users.noreply.github.com>
2019-01-14 17:49:43 +0100

    15A

--- src/recuexamen/RecuExamen.java
+++ src/recuexamen/RecuExamen.java
@@ -12,7 +12,7 @@ public class RecuExamen {
         System.out.println("Això es un hola");
     }
     public static void meitat(){
-        int centre,hola;
+        int centre,hola,aqui_no;
         System.out.println("Aquest està a la meitat");
     }
     public static void fin(){
